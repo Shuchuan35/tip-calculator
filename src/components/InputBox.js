acfd5075c36e575bc38cef7b1b8696dc7f5ea677
scream <hou.jennifer35@gmail.com>
2019-06-03 10:49:19 -0400

    create a react app and push it to gh pages

---
+++ src/components/InputBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const InputBox = () => {
+    return (
+        <div>
+            <input 
+                type='search' 
+                placeholder='enter bill $amount'
+                name='search'
+                >
+            </input>
+        </div>
+    )
+}
+
+export default InputBox;
