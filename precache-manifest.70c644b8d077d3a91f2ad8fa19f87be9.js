03a47bbc70371526ecbf977dc6cb375aeec83cb7
scream <hou.jennifer35@gmail.com>
2019-06-03 10:50:07 -0400

    Updates

---
+++ precache-manifest.70c644b8d077d3a91f2ad8fa19f87be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f0c6e6a5f6533a59d065381b5ff636",
+    "url": "/tip-calculator/index.html"
+  },
+  {
+    "revision": "e7948b6c6fb124b8888a",
+    "url": "/tip-calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/tip-calculator/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "e7948b6c6fb124b8888a",
+    "url": "/tip-calculator/static/js/main.76969982.chunk.js"
+  },
+  {
+    "revision": "db5757b09f3f2a99c5e1",
+    "url": "/tip-calculator/static/js/runtime~main.c6c55241.js"
+  }
+]);
